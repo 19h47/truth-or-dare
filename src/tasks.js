f6dc85a9333ab2f85433fb8145223232f6267f28
Levron Jérémy <jeremylevron@19h47.fr>
2020-03-23 16:24:21 +0100

    :bug: Invert emoji for thruth and dare

--- src/tasks.js
+++ src/tasks.js
@@ -1,317 +1,317 @@
 export default [
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Combien de fois par jour penses-tu à moi ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Avec qui as-tu le plus envie de coucher en ce moment ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Est-ce que tu m’aimes ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Tu pense que nous aurions une chance de coucher si nous étions confinés ensemble ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'As-tu envie de m’arracher mon masque pour m’en rouler une ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Avec combien de personnes as-tu couché ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'M’as tu déjà menti ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Qu’est-ce qui pourra sauver l’amour ?',
 	},
 
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'À quel âge as-tu vécu ton premier orgasme ?',
 	},
 
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Penses-tu que cela va durer avec la personne avec laquelle tu es actuellement ? ',
 	},
 
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Est-ce que tu penses que sur un malentendu, ça peut passer ?',
 	},
 
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Que feras-tu quand les mesures de confinement seront levées ?',
 	},
 
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Quel est, selon toi le plus bel homme de la Terre ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Tu préfères faire l’amour avec ou sans musique ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Avec le confinement, as-tu augmenté ton activité masturbatoire ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'As-tu plus quitté qu’été quitté.e ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Quelle est ta perversion ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Quel est le nom de la personne avec laquelle tu as fait ta première fois ?',
 	},
 
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Tu resterais avec moi si je prenais 50 kilos ? ',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Quelle serait ta réaction si je te disais que j’étais amoureux.euse de toi ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'As-tu déjà regardé son mon portable ?<br>(Je ne te le conseille pas)',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Est-ce que tu m’as trompé.e ?',
 	},
 	{
 		title: 'Vérité (qui dérange)',
-		value: 1,
+		value: 0,
 		content: 'Quelle est ta dernière requête Google ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'Quelle est la partie de ton corps qui est le plus souvent complimentée ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'De qui es-tu amoureux.se ?',
 	},
 	{
 		title: 'Vérité',
-		value: 1,
+		value: 0,
 		content: 'As-tu déj à  commis un délit ?	',
 	},
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Essaie d’attendre 19 heures pour l’apéro',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Fais-moi, s’il te plaît, un top 5 de tes chansons favorites',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Fais-moi , s’il te plaît, un top 5 de tes chansons favorites pour faire l’amour',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Fais-moi, s’il te plaît, un top 5 de tes chansons favorites pour me faire l’amour',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Va te  laver les mains, elles sont dégueulasses',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Reste chez toi',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Tousse dans ton coude or die',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Fais-moi un compliment',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Trouve-moi 5 qualités et surtout, partage-les moi',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Demain, c’est journée sans culotte',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Utilise un moyen technologique actuel pour envoyer « Je t’aime » à quelqu’un de ta famille (avec lequel tu ne couches pas)',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Send nudes',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Send me nudes',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Passe une journée sans boire d’alcool',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Passe une journée sans fumer (si tu m’aimes)',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Dès que tu le pourras, fais-moi toucher tes seins',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Dans une story Instagram publique, dis-moi que tu m’aimes<br>(N’oublie pas de citer Le Bitch Club)',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Via un statut Facebook, annonce à tes contacts que tu m’aimes<br>(N’oublie pas de citer Le Bitch Club)',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Téléphone moi-a-a',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Raconte-moi ce que tu as envie de me faire',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Touche-toi en  pensant à moi',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Fais un classement de des tes en partant de celui que tu aimes le moins pour arriver à celui que tu détestes le plus',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Enregistre-toi en train de me chanter une chanson de Balavoine',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Prends un selfie, envoie-le moi. Tu me manques',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Résiste, prouve que tu existes',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Envoie « Je t’aime mais notre amour est impossible » à<br>la 18 ème personne de ton répertoire téléphonique',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Annonce sur Facebook que tu es enceinte',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Raconte ta plus grosse honte en statut Instagram<br>(Et identifie Le Bitch Club)',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Envoie une bitchline à un ami',
 	},
 
 	{
 		title: 'Action',
-		value: 0,
+		value: 1,
 		content: 'Fais une cure de sébum de 10 jours',
 	},
 ];
