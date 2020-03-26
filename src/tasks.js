af78bbf0f8d0809ae10ddccea547b8e537791402
ines a <62557311+ines-a-coder@users.noreply.github.com>
2020-03-26 22:55:07 +0100

    Update tasks.js

--- src/tasks.js
+++ src/tasks.js
@@ -148,172 +148,172 @@ export default [
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Essaie d’attendre 19 heures pour l’apéro',
+		content: 'Essaie d’attendre 19 heures pour l’apéro !',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Fais-moi, s’il te plaît, un top 5 de tes chansons favorites',
+		content: 'Fais-moi, s’il te plaît, un top 5 de tes chansons favorites !',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Embrasse-moi idiot (après cette épidémie)',
+		content: 'Embrasse-moi idiot (après cette épidémie) !',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Va te laver les mains, elles sont dégueulasses',
+		content: 'Va te laver les mains, elles sont dégueulasses !',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Reste chez toi',
+		content: 'Reste chez toi !',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Tousse dans ton coude or die',
+		content: 'Tousse dans ton coude or die !',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Fais-moi un compliment',
+		content: 'Fais-moi un compliment !',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Énnonce-moi 5 de mes qualités',
+		content: 'Énnonce-moi 5 de mes qualités !',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Demain, c’est sans culotte',
+		content: 'Demain, c’est sans culotte !',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Utilise un moyen technologique actuel pour envoyer « Je t’aime »<br>à quelqu’un de ta famille (avec lequel tu ne couches pas)',
+		content: 'Utilise un moyen technologique actuel pour envoyer « Je t’aime »<br>à quelqu’un de ta famille (avec lequel tu ne couches pas) !',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Send nudes',
+		content: 'Send nudes!',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Send me nudes',
+		content: 'Send me nudes!',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Passe une journée sans boire d’alcool',
+		content: 'Passe une journée sans boire d’alcool !',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Passe une journée sans fumer (si tu m’aimes)',
+		content: 'Passe une journée sans fumer (si tu m’aimes) !',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Dès que tu le pourras, fais-moi toucher tes seins',
+		content: 'Dès que tu le pourras, fais-moi toucher tes seins !',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Dans une story Instagram publique, dis-moi que tu m’aimes<br>(N’oublie pas de citer Le Bitch Club)',
+		content: 'Dans une story Instagram publique, dis-moi que tu m’aimes<br>(N’oublie pas de citer Le Bitch Club) !',
 	},
 
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Si tu crois un jour que tu m\'aimes, n\'attends pas un jour, pas une semaine',
+		content: 'Si tu crois un jour que tu m\'aimes, n\'attends pas un jour, pas une semaine !',
 	},
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Téléphone-moi',
+		content: 'Téléphone-moi !',
 	},
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Raconte-moi ce que tu as envie de me faire',
+		content: 'Raconte-moi ce que tu as envie de me faire !',
 	},
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Pense à moi nu.e',
+		content: 'Pense à moi nu.e !',
 	},
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Ne mange pas de viande pendant une semaine',
+		content: 'Ne mange pas de viande pendant une semaine !',
 	},
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Réalise un classement de des tes ex en partant de celui que<br>tu aimes le moins pour arriver à celui que tu détestes le plus',
+		content: 'Réalise un classement de des tes ex en partant de celui que<br>tu aimes le moins pour arriver à celui que tu détestes le plus !',
 	},
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Enregistre-toi en train de me chanter une chanson de Balavoine',
+		content: 'Enregistre-toi en train de me chanter une chanson de Balavoine !',
 	},
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Prends un selfie et envoie-le moi. Tu me manques',
+		content: 'Prends un selfie et envoie-le moi. Tu me manques !',
 	},
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Résiste, prouve que tu existes',
+		content: 'Résiste, prouve que tu existes !',
 	},
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Envoie « Je t’aime mais notre amour est impossible » à<br>la 18 ème personne de ton répertoire téléphonique',
+		content: 'Envoie « Je t’aime mais notre amour est impossible » à<br>la 18 ème personne de ton répertoire téléphonique !',
 	},
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Annonce sur Facebook que tu es enceinte ou que tu vas être papa',
+		content: 'Annonce sur Facebook que tu es enceinte ou que tu vas être papa !',
 	},
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Évoque plus grosse honte en statut Instagram<br>(identifie Le Bitch Club)',
+		content: 'Évoque plus grosse honte en statut Instagram<br>(identifie Le Bitch Club) !',
 	},
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Envoie une bitchline à un ami',
+		content: 'Envoie une bitchline à un ami !',
 	},
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Embrasse ton ou tes coconfinés (sans prendre de risque pour ta santé)',
+		content: 'Embrasse ton ou tes coconfinés (sans prendre de risque pour ta santé) !',
 	},
 	{
 		title: 'Action',
 		value: 1,
-		content: 'Fais une cure de sébum de 10 jours',
+		content: 'Fais une cure de sébum de 10 jours !',
 	},
 ];
